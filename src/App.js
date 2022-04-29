104512e058e76967195407df23fafaf8de54474a
Saadet-Aliyeva <96382373+Saadet-Aliyeva@users.noreply.github.com>
2022-04-29 06:57:22 +0400

    pricing was ready

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
+import Pricing from './components/Pricing';
 function App () {
 return (
   <>
@@ -13,6 +14,7 @@ return (
  <Navbar />
  <Header />
  <AboutMe />
+ <Pricing />
  </>
   );
 }
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+  
+
+
+const Pricing = ()=> {
+    return(
+        
+        <section className="gg">
+     <div className="price-comparison">
+         <div className="price-column">
+             <div className="price-header">
+               <div className="price">
+                   <div className="dollar-sign">$</div>
+                   10
+                   <div className="por-month">/mo</div>
+                   </div>  
+                   <div className="plan-name">Basic</div>
+             </div>
+             <div className="divider"></div>
+             <div className="feature">
+             Lorem ipsum dolor 
+             </div>
+             
+            <div className="feature">
+            Lorem ipsum dolor 
+             </div>
+             <div className="feature">
+             Lorem ipsum dolor 
+             </div>        
+             
+             <div className="feature">
+             Lorem ipsum dolor 
+             </div>
+             <button className="cta">Buy Now</button>
+         </div>
+
+         <div className="price-column pp">
+             <div className="price-header ">
+               <div className="price">
+                   <div className="dollar-sign">$</div>
+                   20
+                   <div className="por-month">/mo</div>
+                   </div>  
+                   <div className="plan-name">Professional</div>
+             </div>
+             <div className="divider"></div>
+             <div className="feature">
+             Lorem ipsum dolor 
+             </div>
+             
+            <div className="feature">
+            Lorem ipsum dolor 
+             </div>
+             <div className="feature">
+             Lorem ipsum dolor 
+             </div>        
+             
+             <div className="feature">
+                 Lorem ipsum dolor 
+               </div> 
+             <button className="cta-m">Buy Now</button>
+         </div>
+
+         <div className="price-column">
+             <div className="price-header">
+               <div className="price">
+                   <div className="dollar-sign">$</div>
+                   50
+                   <div className="por-month">/mo</div>
+                   </div>  
+                   <div className="plan-name">Enterprise</div>
+             </div>
+             <div className="divider"></div>
+             <div className="feature">
+             Lorem ipsum dolor 
+             </div>
+           
+            <div className="feature">
+            Lorem ipsum dolor 
+             </div>
+             <div className="feature">
+             Lorem ipsum dolor 
+             </div>        
+             
+             <div className="feature">
+             Lorem ipsum dolor 
+             </div>
+            <button className="cta">Buy Now </button>
+           
+         </div>
+     
+         </div>
+        
+         </section>
+    )
+
+
+
+}
+
+export default Pricing
